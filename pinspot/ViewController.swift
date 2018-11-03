b0aa9e70c312ba161814cb6b0c6aee4fea5b6d40
Carlos Marcano <cmarca@gmail.com>
2018-11-03 09:22:30 -0700

    Initial Commit

---
+++ pinspot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pinspot
+//
+//  Created by Carlos Marcano on 11/3/18.
+//  Copyright © 2018 Carlos Marcano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
